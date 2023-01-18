5800b452b792a6e7b2a71938cb6933aafd480db7
Tovy Vaits <antonvaits@gmail.com>
2023-01-18 22:53:31 +0200

    build modal

--- src/utils/getModalContext.js
+++ src/utils/getModalContext.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import { useState, createContext } from "react";
 
-const ModalContext = React.createContext();
+const ModalContext = createContext();
 
 const ModalContextProvider = ({ children }) => {
-  const [modalState, setModalState] = useState(true);
-  const [modalContent, setModalContent] = useState("orderForm");
+  const [modalState, setModalState] = useState(false);
+  const [modalContent, setModalContent] = useState("navbar");
 
   const handleModal = () => setModalState(!modalState);
   const handleModalContent = (contentType) => setModalContent(contentType);
